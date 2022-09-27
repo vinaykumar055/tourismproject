9c3b210da20bf54ace06ffc89059f26bc8f593e5
DELL <DELL@DESKTOP-LILEBQG>
2022-09-27 15:17:50 +0530

    Te-commit

--- src/test/java/githubpackage/CreateTest.java
+++ src/test/java/githubpackage/CreateTest.java
@@ -6,6 +6,12 @@ public class CreateTest {
 @Test
 public void create()
 {
+	
 	System.out.println("---created------");
+}
+public void modify()
+{
+	
+	System.out.println("---created----  modification--");
 }
 }
